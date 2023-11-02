6ab1b0876fa386f8621cdaeeb242b0a233421c36
Carlos_Garay <cgarayh@uni.pe>
2023-11-02 01:56:55 -0500

    created fronted app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    baseURL: "https://employee-managment-app-c9bo.onrender.com"
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    baseURL: "https://employee-managment-app-c9bo.onrender.com"
+}
